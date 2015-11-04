fbfa3f5ca4daca1b83397a41af8b78e26424184f
telucis <ustbwy@126.com>
2015-11-04 23:33:11 +0800

    canvas_project(drawing_board)-20151104
    complete part of HTML,CSS and the init of the drawing.

---
+++ drawing board/js/draw.js
@@ -0,0 +1,17 @@
+//画布
+var canvas;
+var context;
+//蒙版
+var canvas_bak;
+var context_bak;
+
+var canvasWidth=500;
+var canvasHeight=400;
+
+var canvasTop,canvasLeft;
+
+//画笔大小
+var size=1;
+var color='#000';
+
+//画图形
